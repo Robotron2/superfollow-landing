89fa9b295d78e2f3bda2445b4a1aafcd1351aeaa
Theophilus <theophilusadesola002@gmail.com>
2024-03-10 21:59:02 +0100

    🎉  SuperFollow Desktop landing page

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	content: ["./src/**/*.{html,js}"],
+	theme: {
+		extend: {
+			colors: {
+				primary: "#5368f8",
+				light: "#8795fa",
+				secondary: " #F5F6FF",
+				tertiary: "#FD7D53",
+				dark: "#124265",
+			},
+		},
+	},
+	plugins: [],
+};
